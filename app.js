39ef0c27013992fb69ced1f2e5c935d209b7b4be
agustinasc <agustina.santillancastro@gmail.com>
2021-03-19 16:19:10 -0300

    ADD/ Creando grilla y dandole estilos

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-let em = document.getElementById('em');
-const emoji = ['🌞', '🥥', '😎', '🐬', '🌡','🏝',  ]
+let gridBoard = document.getElementById('grid-board');
 
-em.innerHTML = emoji;
 
-var div = document.createElement('div');
-div.textContent = 'I \u2764\uFE0F emoji!';
-document.body.appendChild(div);
+const emojis = ['🌞', '🥥', '😎', '🐬', '🌡', '🏝' ];
+
+//--------- CREANDO LA GRILLA
+
+const creatBoard = () =>{
+    for (let i = 0; i < emojis.length; i++) {
+        for (let j = 0; j < emojis.length; j++) {
+            console.log(i,j)
+           const newp = document.createElement("p");
+           newp.innerHTML = emojis[getRandomNum(6,0)];
+            gridBoard.appendChild(newp);
+        }
+        
+    }
+}
+
+//--------- OBTENIENDO EL NUMERO Y EMOJI RANDOM
+const getRandomNum = (max, min) =>{
+    return Math.floor(Math.random() * (max - min) + min);
+};
+const ramdom = getRandomNum(0, 6);
+
+const getEmoji = console.log(`posicion: ${ramdom}`, emojis[ramdom])
+
+creatBoard();
+
+// ------------TWEMOJI
 
 twemoji.parse(document.body);
 
-var img = div.querySelector('img');
+
+// gridBoard.innerHTML = emojis;
+
+// var div = document.createElement('div');
+// div.textContent = 'I \u2764\uFE0F emoji!';
+// document.body.appendChild(div);
+
+
+// var img = div.querySelector('img');
